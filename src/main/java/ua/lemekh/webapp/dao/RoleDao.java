446413f83e7dec062cbdcd5ceb7f9a8438f5938f
Ostap Lemekh <ostap_10@mail.ru>
2017-05-26 22:31:43 +0300

    update

--- src/main/java/ua/lemekh/webapp/dao/RoleDao.java
+++
@@ -1,7 +0,0 @@
-package ua.lemekh.webapp.dao;
-
-import ua.lemekh.webapp.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleDao extends JpaRepository<Role, Long> {
-}
